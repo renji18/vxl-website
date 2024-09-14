0e1a5bfbb707142b17bbfdaa29b6d51fc0cfa159
renji18 <aadarshjha1401@gmail.com>
2024-09-15 01:26:53 +0530

    final submisstion v1

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
   return (
     <div id="app" className="selection:bg-myPurple overflow-x-hidden">
       <EffectController />
-      {/* <CustomCursor /> */}
+      <CustomCursor />
       <NavBar setContactPop={setContactPop} />
       <Home />
       <div className="h-[250vh] md:h-[170vh] lg:h-[230vh] xl:h-[190vh] w-screen" />
